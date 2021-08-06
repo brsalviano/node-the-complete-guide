3db41bc4d36978619dc6f79551b54a7d947fab69
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-06 17:17:43 -0300

    Objeto, propriedade, e métodos

--- play.js
+++ play.js
@@ -1,27 +1,9 @@
-const name = "Max";
-let age = 29;
-const hasHobbies = true;
-
-//Arrow function
-//O escopo é léxico.
-const summarizeUser = (userName, userAge, userHasHoby) => {
-  return (
-    "Nome é " +
-    userName +
-    ", idade é " +
-    userAge +
-    " e o usuário tem hobbies? " +
-    userHasHoby
-  );
+const person = {
+  name: "Max",
+  age: 29,
+  greet() {
+    console.log("Hi, I am ", this.name);
+  },
 };
 
-//Sintaxe curta de uma arrow function quando há uma única instrução e ela é retornada.
-const add = (a, b) => a + b;
-
-//Se tiver só um parâmetro, pode omitir os parenteses.
-const addOne = (a) => a + 1;
-//Se não tiver parâmetro precisa ter os parenteses.
-
-console.log(summarizeUser(name, age, hasHobbies));
-console.log(add(1, 2));
-console.log(addOne(4));
+person.greet();
