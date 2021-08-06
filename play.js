a865af176b4919a7caeba8939dee7053c6eb8bcb
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-06 17:37:07 -0300

    Spread e Rest operators

--- play.js
+++ play.js
@@ -1,13 +1,17 @@
 const hobbies = ["Sports", "Cooking"];
 
-for (let hobby of hobbies) {
-  console.log(hobby);
+const copiedArray = hobbies.slice();
+console.log(hobbies);
+
+//Criando uma cópia usando spread operator.
+const anotherCopy = [...hobbies];
+//Também funciona com objetos {...nomeObjeto}
+
+
+//Rest operator faz o contrário (junta/merge argumentos em um array).
+const toArray = (...args) => {
+  return args;
 }
 
-console.log(
-  hobbies.map((hobby) => {
-    return "Hobby: " + hobby;
-  })
-);
-
-console.log(hobbies);
+//Podemos passar n parâmetros
+console.log(toArray(1, 2, 3, 4));
