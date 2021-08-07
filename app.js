80f42c2d0f840cad34ee99bcfd5219c1a16a0623
Bruno Salviano da Silva Teixeira <br_salviano@hotmail.com>
2021-08-07 19:03:54 -0300

    Elementos basicos da response

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Hello node</title></head>')
+    res.write('<body><h1>Hello Node</h1></body>')
+    res.write('</html>');
+    res.end(); //finaliza a resposta, depois daqui não pode mais usar res.write.
 
 });
 
